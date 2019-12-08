d0e036e37b29145363600d115ed9537f55e3df6d
Francesco Colasuonno <fcolasuonno@gmail.com>
2019-12-08 18:47:26 +0000

    Day 22 Part 2

--- src/day22/Code.kt
+++ src/day22/Code.kt
@@ -1,6 +1,8 @@
 package day22
 
 import java.io.File
+import java.util.*
+import kotlin.math.abs
 
 fun main() {
     val name = if (false) "test.txt" else "input.txt"
@@ -11,7 +13,34 @@ fun main() {
     println("Part 2 = ${part2(parsed)}")
 }
 
-data class Node(val x: Int, val y: Int, val size: Int, val used: Int, val avail: Int, val use: Int)
+private val xRange = 0..34
+private val yRange = 0..28
+
+data class Node(val x: Int, val y: Int, val size: Int, val used: Int, val avail: Int, val usePercent: Int)
+data class SimpleNode(val pos: Pair<Int, Int>, val size: Int, val used: Int)
+data class NodeMap(val empty: SimpleNode, val goal: SimpleNode, val steps: Int = 0) {
+    var grid: SortedSet<SimpleNode> = sortedSetOf(compareBy<SimpleNode> { it.pos.first }.thenBy { it.pos.second })
+    fun candidates() = setOf(empty.pos.copy(first = empty.pos.first - 1),
+            empty.pos.copy(first = empty.pos.first + 1),
+            empty.pos.copy(second = empty.pos.second - 1),
+            empty.pos.copy(second = empty.pos.second + 1))
+            .filter {
+                it.first in xRange && it.second in yRange
+            }.let { newEmptyPositions ->
+                grid.filter { it.pos in newEmptyPositions }
+            }.mapNotNull { newNode ->
+                if (empty.size >= newNode.used) {
+                    val newEmptyNode = newNode.copy(used = 0)
+                    val simpleNode = empty.copy(used = newNode.used)
+                    this.copy(empty = newEmptyNode, goal = if (newEmptyNode.pos == goal.pos) simpleNode else goal, steps = steps + 1).also {
+                        it.grid.addAll(grid.filter { it.pos != newNode.pos && it.pos != empty.pos } + setOf(newEmptyNode, simpleNode))
+                    }
+                } else null
+            }
+
+    fun emptyToGoal() = abs(empty.pos.first - xRange.last - 1) + empty.pos.second
+    fun goalToZero() = goal.pos.first + goal.pos.second
+}
 
 private val lineStructure = """/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%""".toRegex()
 
@@ -28,4 +57,39 @@ fun part1(input: Set<Node>): Any? = input.filter { it.used != 0 }.sumBy { node -
     (input - node).count { node.used <= it.avail }
 }
 
-fun part2(input: Set<Node>): Any? = input.size
+fun part2(input: Set<Node>): Int {
+    val newInput = input.map { SimpleNode(it.x to it.y, it.size, it.used) }
+    var solution: NodeMap? = null
+    val candidates = NodeMap(
+            newInput.single { it.used == 0 },
+            newInput.single { it.pos.first == xRange.last && it.pos.second == 0 }
+    ).also { it.grid.addAll(newInput) }.candidates()
+            .toSortedSet(compareBy<NodeMap> { it.steps + it.emptyToGoal() }
+                    .thenBy { it.empty.pos.first }.thenBy { it.empty.pos.second })
+    val seen = mutableSetOf<Pair<Int, Int>>()
+    while (candidates.isNotEmpty()) {
+        val first = candidates.first()
+        seen.add(first.empty.pos)
+        candidates.remove(first)
+        if (first.emptyToGoal() == 1) {
+            solution = first
+        } else {
+            candidates.addAll(first.candidates().filter { it.empty.pos !in seen })
+        }
+    }
+
+    val newCandidates = solution!!.candidates().toSortedSet(compareBy<NodeMap> { it.goal.pos.first }
+            .thenBy { it.goal.pos.second }
+            .thenBy { it.steps }
+            .thenBy { it.empty.pos.first }.thenBy { it.empty.pos.second })
+    while (newCandidates.isNotEmpty()) {
+        val first = newCandidates.first()
+        newCandidates.remove(first)
+        if (first.goalToZero() == 0) {
+            return first.steps
+        } else {
+            newCandidates.addAll(first.candidates())
+        }
+    }
+    return 0
+}
